1515eefe16efb8a51b029d7c2f69cd66c715cb45
Grant Mitchell <soundbreakr1@gmail.com>
2022-12-20 20:30:12 -0700

    using rest key

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,6 +1,7 @@
 module.exports = ({github, context}) => {
-  console.log('github issues',Object.keys(github));
+  console.log('github issues',github.rest.issues);
   //context.issues
+  //rest,request
   // console.log('context',context);
   // https://api.github.com/repos/foo/bar/issues
   return github;
