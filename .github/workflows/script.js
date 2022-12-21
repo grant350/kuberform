6a30fffeebe669bb28b0a187453c6bf328101205
Grant Mitchell <soundbreakr1@gmail.com>
2022-12-20 20:26:32 -0700

    code not changed

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,5 +1,5 @@
 module.exports = ({github, context}) => {
-  console.log('github context',github.issues);
+  console.log('github issues',github.issues);
   // console.log('context',context);
   // https://api.github.com/repos/foo/bar/issues
   return github;
