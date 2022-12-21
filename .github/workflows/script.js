a79f82f611632da6217496997b889e2e00d9392c
Grant Mitchell <soundbreakr1@gmail.com>
2022-12-20 20:24:25 -0700

    listing all issues

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,5 +1,6 @@
 module.exports = ({github, context}) => {
-  console.log('github',github);
-  console.log('context',context);
+  console.log('github context',github.issues);
+  // console.log('context',context);
+  // https://api.github.com/repos/foo/bar/issues
   return github;
 }
