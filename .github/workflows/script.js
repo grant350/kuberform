9072bd90ce3261be44bd292e0b0922c2603e360a
Grant Mitchell <soundbreakr1@gmail.com>
2022-12-20 21:02:36 -0700

    logging label

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,7 +1,8 @@
-module.exports = ({github, context}) => {
+module.exports = async ({github, context}) => {
   console.log('github issues',github.rest.issues);
   const getLabel = github.rest.issues.getLabel;
-  var label = getLabel({"owner":"grant350","name":"patch","repo":"kuberform"});
+  var label = await getLabel({"owner":"grant350","name":"patch","repo":"kuberform"});
+  console.log('label',label);
   //context.issues
   //rest,request
   // console.log('context',context);
