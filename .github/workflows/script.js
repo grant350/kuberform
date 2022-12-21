29a3ea97d4456ddf6b6555c0d99328dd866aa2cb
Grant Mitchell <soundbreakr1@gmail.com>
2022-12-20 20:28:51 -0700

    listing keys

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,5 +1,6 @@
 module.exports = ({github, context}) => {
-  console.log('github issues',github.context.issues);
+  console.log('github issues',Object.keys(github));
+  //context.issues
   // console.log('context',context);
   // https://api.github.com/repos/foo/bar/issues
   return github;
