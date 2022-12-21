66bedb67f132fd93c3299b23161c0cf62c8466ce
Grant Mitchell <soundbreakr1@gmail.com>
2022-12-20 21:16:56 -0700

    eventlist fn not workign

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,10 +1,10 @@
 module.exports = async ({github, context}) => {
   console.log('github issues',github.rest.issues.events);
-  const getIssues = github.rest.issues.listEvents;
+  var getIssues = github.rest.issues.listEvents;
 
   //context.issues
   //rest,request
-  console.log('getIssues',getIssues({owner:"grant350",repo:"kuberform"}));
+  console.log('getIssues',getIssues({owner:"grant350",repo:"kuberform",page:1,per_page:5}));
   // https://api.github.com/repos/foo/bar/issues
   return github;
 }
